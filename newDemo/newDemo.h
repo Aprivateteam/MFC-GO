58658279de34daee120ac2fb6b5ac5b3da3f9544
SU-vito <490366759@qq.com>
2017-09-08 15:07:20 +0800

    新工程

---
+++ newDemo/newDemo.h
@@ -0,0 +1,32 @@
+
+// newDemo.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CnewDemoApp: 
+// 有关此类的实现，请参阅 newDemo.cpp
+//
+
+class CnewDemoApp : public CWinApp
+{
+public:
+	CnewDemoApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CnewDemoApp theApp;
